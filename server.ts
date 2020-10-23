785f681fc04f8cfb048c6b719c5b92b6919f7d60
Igor Cruz <igor_cruz_avelino@hotmail.com>
2020-10-22 21:21:23 -0300

    Change package

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
-// server.js
 import express from 'express';
+import path from 'path';
 import serveStatic from 'serve-static';
 
 const app = express();
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8080;
 
 app.listen(port);
+console.log(`app is listening on port: ${port}`);

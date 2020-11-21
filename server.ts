10afb466929c71763091c415afd5ebee3ce1e0eb
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2020-11-20 23:20:11 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,15 +2,6 @@ import express from 'express';
 import path from 'path';
 import serveStatic from 'serve-static';
 
-import Vue from 'vue';
-import VueTheMask from 'vue-the-mask';
-import App from '.src/App.vue';
-import router from '.src/router/routes';
-import store from '.src/store';
-import axios from '.src/plugins/axios';
-import vuetify from '.src/plugins/vuetify';
-
-
 const app = express();
 
 app.use(serveStatic(path.join(__dirname, '/dist')));
@@ -18,15 +9,3 @@ const port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log(`app is listening on port: ${port}`);
-
-
-Vue.config.productionTip = false;
-Vue.prototype.$http = axios;
-Vue.use(VueTheMask);
-
-new Vue({
-    router,
-    store,
-    vuetify,
-    render: h => h(App),
-}).$mount('#app');
